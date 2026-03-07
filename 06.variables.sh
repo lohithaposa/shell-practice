293c9481ebd76150973423dd835fb3ee5e2afabb
Lohitha <posalohitha189@gmail.com>
2026-03-07 12:39:51 +0530

    actions

--- 06.variables.sh
+++ 06.variables.sh
@@ -7,5 +7,5 @@ sleep 10
 
 END_TIME=$(date +%s)
 
-TOTAL_TIME=$(($END_TIME - $STARTTIME)) #One brace is for mathematics exceution and othe brace is for command exceution
+TOTAL_TIME=$(($END_TIME-$STARTTIME)) #One brace is for mathematics exceution and othe brace is for command exceution
 echo "Script exceuted in: $TOTAL_TIME
