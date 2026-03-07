99f78fc41810571707a1484998f9306f948f933f
Lohitha <posalohitha189@gmail.com>
2026-03-07 11:19:25 +0530

    actions

---
+++ 04variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: I am doing good. Whats up?"
+echo "$PERSON2:: Nothing"
