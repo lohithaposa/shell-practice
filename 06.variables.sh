12ea32e8859008c9931f664f7cd455f698beba98
Lohitha <posalohitha189@gmail.com>
2026-03-07 12:37:48 +0530

    actions

--- 06.variables.sh
+++ 06.variables.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-STARTTIME=$(date)
+START_TIME=$(date +%s)
 echo "Script executed at :$TIMESTAMP"
 
 sleep 10
 
-END_TIME=$(date)
+END_TIME=$(date +%s)
+
+TOTAL_TIME=$(($END_TIME - $STARTTIME)) #One brace is for mathematics exceution and othe brace is for command exceution
+echo "Script exceuted in: $TOTAL_TIME
