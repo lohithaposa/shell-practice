92778cd4e565b56101ea3aa05070bedb5da85b97
Lohitha <posalohitha189@gmail.com>
2026-03-11 17:25:47 +0530

    actions

--- 23.deleteoldlogs.sh
+++ 23.deleteoldlogs.sh
@@ -9,12 +9,14 @@ LOGS_DIRECTORY=/home/ec2-user/app-logs
 
 if [ ! d  $LOGS_DIRECTORY ] ; then
    echo  -e " $LOGS_DIRECTORY does not exist"
-   exit
+   exit 1
 fi   
 
-FILES_TO_DELETE=$( find $LOGS_DIRECTORY -name "*.log" -type f -mtime +14)
+FILES_TO_DELETE=$(find $LOGS_DIRECTORY -name "*.log" -type f -mtime +14)
 echo "$FILES_TO_DELETE"
 
-while IFS= read -r line; do
- echo "$line"
+while IFS= read -r filepath; do
+ echo "DEleting the file : $filepath"
+ rm -f $filepath
+ echo "deleting file : $filepath"
 done <<< $FILES_TO_DELETE
