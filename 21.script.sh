0187ca0f7a216b54ee93a5c345d42089adb91d80
Lohitha <posalohitha189@gmail.com>
2026-03-11 17:37:15 +0530

    actions

--- 21.script.sh
+++ 21.script.sh
@@ -3,5 +3,12 @@ COUNTRY=INDIA
 echo "I am in: $COUNTRY"
 echo "PID of SCRIPT-1: $$"
 
-sh 22.script2.sh
-#source ./22-script-2.sh
+#sh 22.script2.sh
+
+# o/p: I am in: INDIA
+# PID of SCRIPT-1: 27599
+# I am in:
+# PID of SCRIPT-2: 27600
+
+
+source ./22-script-2.sh
