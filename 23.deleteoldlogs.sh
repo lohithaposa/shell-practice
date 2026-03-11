7aa5ba9f5c02b21ef21fe288de10bd72026bb31d
Lohitha <posalohitha189@gmail.com>
2026-03-11 16:57:06 +0530

    actions

---
+++ 23.deleteoldlogs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+
+LOGS_DIRECTORY=/home/ce2-user/app-logs
+
+if [ ! d  $LOGS_DIRECTORY ] ; then
+   echo  -e " $LOGS_DIRECTORY does not exist"
+   exit
+fi   
+
+FILES_TO_DELETE=$( find $LOGS_DIRECTORY -name "*.log" -type f -mtime +14)
+echo "$FILES_TO_DELETE"
