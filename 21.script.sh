75f0257356323784edc876b64fbcdfe41db89ec4
Lohitha <posalohitha189@gmail.com>
2026-03-11 17:39:08 +0530

    actions

--- 21.script.sh
+++ 21.script.sh
@@ -4,11 +4,11 @@ echo "I am in: $COUNTRY"
 echo "PID of SCRIPT-1: $$"
 
 #sh 22.script2.sh
-
+#process 2 cannnot access process1
 # o/p: I am in: INDIA
 # PID of SCRIPT-1: 27599
 # I am in:
 # PID of SCRIPT-2: 27600
 
 
-source ./22-script-2.sh
+source ./22.script2.sh
