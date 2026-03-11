12020469b80832d3a119d00cb1f3b9d071ab8748
Lohitha <posalohitha189@gmail.com>
2026-03-11 16:54:28 +0530

    actions

---
+++ 21.script.sh
@@ -0,0 +1,7 @@
+COUNTRY=INDIA
+
+echo "I am in: $COUNTRY"
+echo "PID of SCRIPT-1: $$"
+
+sh 22-script-2.sh
+#source ./22-script-2.sh
---
+++ 24.whileloop.sh
@@ -0,0 +1,9 @@
+count=1
+
+while [ $count -le 5 ]
+do
+  echo "Count is $count"
+  sleep 1
+  # Increment the counter
+  ((count++)) 
+done
